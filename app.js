e6e9bc958eb9429a8a3822943395b94ccfedc0ed
Alex Saavedra <fass28@icloud.com>
2022-12-22 19:33:20 -0500

    added post for the compose

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = []
 
 app.set('view engine', 'ejs');
 
@@ -37,6 +38,23 @@ app.get('/compose', function(req,res){
   res.render('compose')
 })
 
+
+app.post('/compose', function(req,res){
+  let composeTittle = req.body.composeTittle
+  let composeText = req.body.composeText
+  let post = {}
+
+  post['tittle'] = composeTittle
+  post['text'] = composeText
+
+  posts.push(post)
+  res.redirect('/')
+
+
+  console.log(post.tittle)
+  console.log(post.text)
+  console.log(posts);
+})
 
 
 
